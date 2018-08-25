a5f3fbebbd01d90aa4bdb6bd5ad3547f1f6abf75
Hooterr <maxus1999@gmail.com>
2018-08-25 19:24:11 +0200

    Test

--- Backend/Pogodeo/Startup.cs
+++ Backend/Pogodeo/Startup.cs
@@ -21,6 +21,7 @@ namespace Pogodeo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //TEST
             services.AddMvc();
             services.AddDbContext<PogodeoAppDataContext>(options =>
             {
